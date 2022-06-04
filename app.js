1681b09291ce9b5859bc1f2e0c99348f01c3e1b6
unjinjang <alpsboy1996@gmail.com>
2022-06-04 18:20:52 +0900

    aram test

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
+console.log("from aram");
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
