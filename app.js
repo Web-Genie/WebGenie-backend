fae7b33ac0a33f1978124898f43d9a0d3b5193bf
unjinjang <alpsboy1996@gmail.com>
2022-06-04 18:16:45 +0900

    intial commit edit

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var usersRouter = require("./routes/users");
 var app = express();
 
 console.log("12");
+console.log("12");
+console.log("12");
+console.log("12");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
