dcea9526c6a4fd0bf59f9d37cf8cb9a6ab3357eb
inbeen <blueline1318@gmail.com>
2022-06-07 13:12:03 +0900

    [REFACTOR]: Delete cookie-parser

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require("./config/firebaseAdmin");
 
 const createError = require("http-errors");
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const app = express();
@@ -13,7 +12,6 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 const loginRouter = require("./routes/login");
 const websitesRouter = require("./routes/websites");
@@ -30,7 +28,11 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  console.error(err.stack);
+  console.error({
+    "error status :": err.stauts,
+    "error message :": err.message,
+    "error stack :": err.stack
+  });
 });
 
 module.exports = app;
