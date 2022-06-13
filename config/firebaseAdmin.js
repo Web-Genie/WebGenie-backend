41515c6bd4cdceb80f73b43a14545d143d7e7505
unjinjang <alpsboy1996@gmail.com>
2022-06-14 00:52:08 +0900

    [FEAT]: seperate process.env

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,8 +1,18 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 const firebaseAdminApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.TYPE,
+    projectId: process.env.PROJECT_ID,
+    privateKeyId: process.env.PRIVATE_KEY_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    clientEmail: process.env.CLIENT_EMAIL,
+    clientId: process.env.CLIENT_ID,
+    authUri: process.env.AUTH_URI,
+    tokenUri: process.env.TOKEN_URI,
+    authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
+  }),
 });
 
 exports.firebaseAdminAuth = firebaseAdminApp.auth();
