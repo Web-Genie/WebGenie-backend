44d4ad868668fc17a22dd04cfa4e137ad0d9f0f3
rami <aarmakrr@hanmail.net>
2022-06-14 03:20:21 +0900

    [REFACTOR]: Refactor image controller

--- routes/image.js
+++ routes/image.js
@@ -3,6 +3,7 @@ const imageRouter = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const { postImage } = require("../routes/controllers/image.controller");
+
 imageRouter.post("/", upload.single("imageFile"), postImage);
 
 module.exports = imageRouter;
