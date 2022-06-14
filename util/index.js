0b79cecad451d1de55b9f5cd902487cc0e0d0cb9
rami <aarmakrr@hanmail.net>
2022-06-15 03:40:45 +0900

    [REFACTOR]: Refactor image controller

---
+++ util/index.js
@@ -0,0 +1,25 @@
+const S3 = require("aws-sdk/clients/s3");
+const fs = require("fs");
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION;
+const bucketName = process.env.AWS_BUCKET_NAME;
+
+const s3 = new S3({
+  region: region,
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+});
+
+const upload = (file) => {
+  const fileStream = fs.createReadStream(file.path);
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  return s3.upload(uploadParams).promise();
+};
+
+module.exports = { upload };
