59fbaa61a93dd6082ce52a3951e6d939aabe78be
rami <aarmakrr@hanmail.net>
2022-06-15 02:03:41 +0900

    [REFACTOR]: Modify postImage controller

--- routes/controllers/image.controller.js
+++ routes/controllers/image.controller.js
@@ -1,21 +1,29 @@
-const { upload } = require("../controllers/aws.controller");
+const fs = require("fs");
 const {
   S3Client,
   PutObjectCommand,
   DeleteObjectCommand,
 } = require("@aws-sdk/client-s3");
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_REGION;
-
 const newS3 = new S3Client({
-  region: region,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  region: process.env.AWS_REGION,
 });
 
 exports.postImage = async (req, res, next) => {
   try {
     const image = req.file;
-    const result = await upload(image);
+
+    const params = {
+      Bucket: process.env.AWS_ACCESS_KEY_ID,
+      Body: fs.createReadStream(image.path),
+      Key: image.filename,
+    };
+
+    await newS3.send(new PutObjectCommand(params));
 
     res.status(201).json({ location: result.Location });
   } catch (error) {
@@ -31,7 +39,7 @@ exports.deleteImage = async (req, res, next) => {
     for (let i = 0; i < splitedUrl.length; i++) {
       await newS3.send(
         new DeleteObjectCommand({
-          Bucket: bucketName,
+          Bucket: process.env.AWS_BUCKET_NAME,
           Key: splitedUrl[i],
         })
       );
